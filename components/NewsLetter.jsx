903e4732a8179e8223882ba7ec01f4c5378db63d
AshiqueHaneef <ashiixc@gmail.com>
2023-08-16 11:19:03 -0700

    latest build

--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -7,7 +7,7 @@ import React from 'react'
 function NewsLetter() {
 	return (
 		<div className="flex lg:flex-row bg-red-600 flex-col lg:justify-between justify-center items-center lg:h-auto  ">
-			<div className=" w-full lg:w-1/2 flex justify-center">
+			
 			<Image src={supportImage} className=' h-full w-auto md:w-full lg:w-auto pb-7 lg:pb-0' alt="support image"/>
 
 				{/* <form className="pt-3 px-4 md:px-0">
@@ -15,7 +15,7 @@ function NewsLetter() {
 						<input className="border border-white py-1.5 px-4 focus:outline-none focus:border-black bg-transparent text-white placeholder-slate-800 w-full md:w-1/2" placeholder="Your Email Address" />
 						<button className="bg-white text-green-900 p-2 mt-2">Submit</button>
 					</form> */}
-			</div>
+		
 			<div className='w-full lg:w-1/2 bg-red-600 justify-center flex flex-col items-center h-auto my-6 px-5 md:my-0 md:px-0'><Link href={'/ContactUs'} className='md:w-2/3 md:pl-8'>
 				<div className="text-white   md:text-justify  text-lg md:text-2xl lg:text-2xl ">
 					Our dedicated support team is here to help you. If you have any questions, please reach out to us and we&apos;ll do our best to assist you.
